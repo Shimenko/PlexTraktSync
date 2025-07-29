8966975eb2913d87560db85c4908eecebbd8d2a2
Fiks <fiks@merakisoft.io>
2025-07-29 16:44:40 -0400

    playing with higher throttle limits

--- plextraktsync/decorators/throttle.py
+++ plextraktsync/decorators/throttle.py
@@ -9,7 +9,7 @@ from plextraktsync.factory import logging
 logger = logging.getLogger(__name__)
 
 class RateLimiter:
-    def __init__(self, calls_per_second=3):
+    def __init__(self, calls_per_second=10):
         self.calls_per_second = calls_per_second
         self.interval = 1.0 / calls_per_second
         self.last_call_time = 0
